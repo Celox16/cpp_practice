e76042d4114d1d618f057c2f8f35a9333456ff0a
Celox16 <sck04184@naver.com>
2021-07-23 15:43:23 +0900

    클래스 탬플릿 예시

--- cpp_practice/source.cpp
+++ cpp_practice/source.cpp
@@ -4,27 +4,19 @@
 using namespace std;
 
 template <typename T>
-void change(T& a, T& b) {
-	T temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
-
-template<>
-void change<int>(int& a, int& b) {
-	cout << "integer data change.\n";
-	int temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
+class Data {
+private:
+	T data;
+public:
+	Data(T data) : data(data) {}
+	void setData(T data) { this->dta = data; }
+	T getData() { return data; }
+};
 
 int main(void) {
-	int a = 7;
-	int b = 5;
-	change(a, b);
-	cout << a << ":" << b << endl;
+	Data<int> data1(1);
+	Data<string>data2("¹Ú°­¹Î");
+	cout << data1.getData() << ":" << data2.getData() << '\n';
 
 	return 0;
 }
