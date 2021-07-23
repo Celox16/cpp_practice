3fa5ea5d0cacbb2bc89c63614ce6cd42361833a1
Celox16 <sck04184@naver.com>
2021-07-23 14:00:45 +0900

    C++ 함수 오버로딩

--- cpp_practice/source.cpp
+++ cpp_practice/source.cpp
@@ -14,35 +14,18 @@ class Person {
 private:
 	string name;
 public:
+	Person() { name = "홍길동"; }
 	Person(string name) : name(name) { }
-	string getName() {
-		return name;
-	}
 	void showName() {
-		cout << "이름 : " << getName() << '\n';
-	}
-};
-
-class Student : Person, public Temp {
-private:
-	int studentID;
-public:
-	Student(int studentID, string name) : Person(name) {
-		this->studentID = studentID;
-	}
-	void show() {
-		cout << "학생 번호 : " << studentID << '\n';
-		cout << "학생 이름 : " << getName() << '\n';
-	}
-	void showName() {
-		cout << "학생 이름 : " << getName() << '\n';
+		cout << "이름 : " << name << '\n';
 	}
 };
 
 int main(void) {
-	Student student(1, "박강민");
-	student.showName();
-	student.showTemp();
+	Person person1;
+	person1.showName();
+	Person person2("박강민");
+	person2.showName();
 
 	return 0;
 }
