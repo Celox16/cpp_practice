2560b6e21c561e38f35ceb1238afc540729f1ba0
Celox16 <sck04184@naver.com>
2021-07-23 14:46:29 +0900

    상수 멤버의 예제

--- cpp_practice/source.cpp
+++ cpp_practice/source.cpp
@@ -6,20 +6,26 @@ using namespace std;
 
 class Person {
 private:
+	const int id;
 	string name;
 public:
 	static int count;
-	Person(string name) : name(name) {
+	Person(int id, string name) : id(id), name(name) {
 		count++;
 	}
+	/*
+	* void setId(int id{
+	*	this->id = id; // 오류 발생 [수정 불가능]
+	* }
+	*/
 };
 
 int Person::count = 0;
 
 int main(void) {
-	Person p1("박강민");
-	Person p2("홍길동");
-	Person p3("임꺽정");
+	Person p1(1, "박강민");
+	Person p2(2, "홍길동");
+	Person p3(3, "임꺽정");
 	cout << "사람의 수 : " << Person::count << '\n';
 
 	return 0;
