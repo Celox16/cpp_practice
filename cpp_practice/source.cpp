d93c2aaea0611c65c373a4dda6c0481bcf2f7711
Celox16 <sck04184@naver.com>
2021-07-23 17:40:06 +0900

    STL 연관 컨테이너 map 예시

--- cpp_practice/source.cpp
+++ cpp_practice/source.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <set>
+#include <map>
 
 using namespace std;
 
 int main(void) {
-	int array[5] = { 2, 4, 6, 8, 10 };
-	set<int> s(array, array + 5);
-	set<int>::iterator iter = s.begin();
-	for (; iter != s.end(); iter++) {
-		cout << *iter << ' ';
+	map<string, int> m;
+	m["박강민"] = 1; m["홍길동"] = 2; m["임꺽정"] = 3;
+	map<string, int>::iterator iter = m.begin();
+	for (; iter != m.end(); iter++) {
+		cout << iter->first << ":" << iter->second << endl;
 	}
-	cout << endl;
-	s.insert(1);
-	s.insert(3);
-	s.insert(5);
-	iter = s.begin();
-	for (; iter != s.end(); iter++) {
-		cout << *iter << ' ';
+	m["이순신"] = 4;
+	cout << m["없는 사람"] << endl;
+	iter = m.begin();
+	for (; iter != m.end(); iter++) {
+		cout << iter->first << ":" << iter->second << endl;
 	}
-	cout << endl;
 
 	return 0;
 }
