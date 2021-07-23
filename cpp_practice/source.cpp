f59e232bce55acde34ec89c16b6419defe579d52
Celox16 <sck04184@naver.com>
2021-07-23 16:21:20 +0900

    STL 컨테이너 Queue 예시

--- cpp_practice/source.cpp
+++ cpp_practice/source.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <stack>
+#include <queue>
 
 using namespace std;
 
 int main(void) {
-	stack<int> s;
-	s.push(7); s.push(5); s.push(4); s.pop(); s.push(6); s.pop();
-	while (!s.empty()) {
-		cout << s.top() << ' ';
-		s.pop();
+	queue<int> q;
+	q.push(7); q.push(5); q.push(4); q.pop(); q.push(6); q.pop();
+	while (!q.empty()) {
+		cout << q.front() << ' ';
+		q.pop();
 	}
 
 	return 0;
