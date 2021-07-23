80826a7cb81512450722aea9e7bc02db7916c83a
Celox16 <sck04184@naver.com>
2021-07-23 17:01:59 +0900

    STL 시퀀스 컨테이너 덱 예시

--- cpp_practice/source.cpp
+++ cpp_practice/source.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
-#include <queue>
+#include <deque>
 
 using namespace std;
 
 int main(void) {
-	int n, x;
-	cin >> n;
-	priority_queue<int> pq;
-	for (int i = 0; i < n; i++) { cin >> x; pq.push(x); }
-	while (!pq.empty()) {
-		cout << pq.top() << ' ';
-		pq.pop();
-	}
+	deque<int> d;
+	d.push_front(3); d.push_back(7); d.pop_front(); d.push_front(4);
+	for (int i = 0; i < d.size(); i++) { cout << d[i] << ' '; }
+	cout << endl;
+	deque<int>::iterator iter;
+	iter = d.begin();
+	d.insert(iter + 1, 3, 5);
+	iter = d.begin();
+	d.insert(iter + 1, 1, 9);
+	for (int i = 0; i < d.size(); i++) { cout << d[i] << ' '; }
+	cout << endl;
+	d.clear();
+	cout << d.empty() << endl;
 
 	return 0;
 }
