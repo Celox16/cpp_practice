5490f32616844595f17d2b90e150f763ead40714
Celox16 <sck04184@naver.com>
2021-07-23 15:29:17 +0900

    탬플릿의 간단한 예시

--- cpp_practice/source.cpp
+++ cpp_practice/source.cpp
@@ -1,32 +1,21 @@
 #include <iostream>
 #include <string>
-#include <ctime> // time이나 locatime함수 라이브러리
 
 using namespace std;
 
-class Person {
-private:
-	const int id;
-	string name;
-public:
-	static int count;
-	Person(int id, string name) : id(id), name(name) {
-		count++;
-	}
-	/*
-	* void setId(int id{
-	*	this->id = id; // 오류 발생 [수정 불가능]
-	* }
-	*/
-};
-
-int Person::count = 0;
+template <typename T>
+void change(T& a, T& b) {
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
 
 int main(void) {
-	Person p1(1, "박강민");
-	Person p2(2, "홍길동");
-	Person p3(3, "임꺽정");
-	cout << "사람의 수 : " << Person::count << '\n';
+	string a = "박강민";
+	string b = "홍길동";
+	change(a, b);
+	cout << a << ":" << b << endl;
 
 	return 0;
 }
